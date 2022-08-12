d9cfb965c446365f46c82c5f2ce36e25280cd3db
NavaneethSuresan <navaneeth@techis.io>
2022-08-12 16:42:11 +0530

    finished

--- src/App.js
+++ src/App.js
@@ -1,51 +1,13 @@
 
 import './App.css';
 import Counter from './Counter';
-import Employee from './Employee';
 
-
-const employeeInfo=[
-    {
-      firstName:'Jack',
-      lastName:'Hamilton',
-      age:'24',
-      id:'1'
-    },
-    {
-      firstName:'Jane',
-      lastName:'Watson',
-      age:'22',
-      id:'2'
-    },
-    {
-      firstName:'Andrew',
-      lastName:'Garfield',
-      age:'28',
-      id:'3'
-    },
-    {
-      firstName:'Thomas',
-      lastName:'Wayne',
-      age:'34',
-      id:'4'
-    }
-  ];
 
 function App() {
-  const name='hello';
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Company Directory</h1>
-        {employeeInfo.map(employee=>{
-          
-          return(
-            <Employee key={employee.id} {...employee}/>
-          );
-        })}
-
-
-      </header>
+      <Counter/>
     </div>
   );
 }
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,20 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Counter=()=>{
-  const [count,setCount]=useState(0);
-  
-  return(
-    <React.Fragment>
-      <p>
-        The count is: {count}
-      </p>
-      <button onClick={()=>setCount(count+1)}>Increment</button>
-      <button onClick={()=>setCount(count-1)}>Decrement</button>
-      <button onClick={()=>setCount(count*2)}>Twice</button>
-      <button onClick={()=>setCount(count/2)}>Halfed</button>
-    </React.Fragment>
-  )
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState(0);
 
+  return (
+    <>
+      <div className="counter">
+        <button className="btn" onClick={() => setCount(count + 1)}>Increment by 1</button>
+        <br />
+        <button className='btn' onClick={() => setCount(count - 1)}>Decrement by 1</button>
+        <br />
+        <div className="by">
+          <button onClick={() => setCount(count + input)}>
+            Add by {input}
+          </button>
+          <input onChange={(e) => setInput(+e.target.value)} />
+        </div>
+      </div>
+
+      <h1>Count: {count}</h1>
+    </>
+  );
 };
- 
-  export default Counter
+
+export default Counter;
