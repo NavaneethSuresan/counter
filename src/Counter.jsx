366076589c2f7bee41ac14e5a996501a3901aaca
NavaneethSuresan <navaneeth@techis.io>
2022-08-25 18:33:41 +0530

    redux

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,21 +1,41 @@
 import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addCount,subCount } from "./redux/action";
 
 const Counter = () => {
-  const [count, setCount] = useState(0);
-  const [input, setInput] = useState(0);
+  // const [count, setCount] = useState(0);
+  const count=useSelector(state=>state.count)
+  const dispatch=useDispatch()
+  const [input, setInput] = useState();
 
   return (
     <>
       <div className="counter">
-        <button className="btn" onClick={() => setCount(count + 1)}>Increment by 1</button>
+        <button className="btn" onClick={() => dispatch(addCount())}>
+          Increment by 1
+        </button>
         <br />
-        <button className='btn' onClick={() => setCount(count - 1)}>Decrement by 1</button>
+        <button className="btn" onClick={() => dispatch(subCount())}>
+          Decrement by 1
+        </button>
         <br />
-        <div className="by">
+        {/* <div className="by">
           <button onClick={() => setCount(count + input)}>
             Add by {input}
           </button>
-          <input onChange={(e) => setInput(+e.target.value)} />
+          <input onChange={(e)=>{
+            
+           setInput(+e.target.value)}}/>
+        </div> */}
+        <div className="by">
+          <button onClick={()=>dispatch(addCount(input))} >
+            Add by {input}
+          </button>
+          <input
+            onChange={(e) => {
+              setInput(+e.target.value);
+            }}
+          />
         </div>
       </div>
 
